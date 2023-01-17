3e244994272ff076ed516c768869a85c9cd989b9
ChurkinaMV <ChurkinaMV@npoa.org>
2023-01-17 13:01:50 +0500

    правки в adaptQuestLevel

--- project/src/utils.ts
+++ project/src/utils.ts
@@ -1,8 +1,10 @@
+import { QUEST_LEVELS } from './const';
+
 export const adaptQuestLevel = (questLevel: string) => {
   switch (questLevel) {
-    case 'easy': return 'Легкий';
-    case 'medium': return 'Средний';
-    case 'hard': return 'Сложный';
+    case QUEST_LEVELS.easy: return 'Легкий';
+    case QUEST_LEVELS.medium: return 'Средний';
+    case QUEST_LEVELS.hard: return 'Сложный';
     default: return 'Любой';
   }
 };
